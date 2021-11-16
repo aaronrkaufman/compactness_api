d6ff2f70bc7f0358a581e269487492c5f2ff1168
Aaron Kaufman <ak8096@nyu.edu>
2021-11-16 11:17:37 +0400

    Update plumber.R

--- plumber.R
+++ plumber.R
@@ -19,6 +19,8 @@ library(plumber)
 library(compactness)
 library(Rook)
 library(textcat)
+library(uuid)
+library(sf)
 
 #* Log system time, request method and HTTP user agent of the incoming request
 #* @filter logger
@@ -37,12 +39,38 @@ function(req){
 }
 
 #* Get compactness
+#* @param shp The shp file
+#* @param dbf The dbf file
+#* @param shx The shx file
+#* @param prj The projection file
+#* @param namecol A string denoting the identifier column
 #* @post /compact
 #* @serializer json
 function(req) {
   tmp = Rook::Multipart$parse(req)
-  shp = tmp$data$filename
+  #print(str(tmp))
+
+  fn = uuid::UUIDgenerate()
+  
+  shx1 = tmp$shx$tempfile
+  shp1 = tmp$shp$tempfile
+  dbf1 = tmp$dbf$tempfile
+  prj1 = tmp$prj$tempfile
+  
+  ## rename shx1 and shp1
+  shx2 = stringr::str_replace(shx1, "Multipart.*", paste0(fn, ".shx"))
+  shp2 = stringr::str_replace(shp1, "Multipart.*", paste0(fn, ".shp"))
+  dbf2 = stringr::str_replace(dbf1, "Multipart.*", paste0(fn, ".dbf"))
+  prj2 = stringr::str_replace(prj1, "Multipart.*", paste0(fn, ".prj"))
+  
+  file.rename(shx1, shx2)
+  file.rename(shp1, shp2)
+  file.rename(dbf1, dbf2)
+  file.rename(prj1, prj2)
+  
+  #shp = tmp$data$tempfile
   namecol = tmp$namecol
+  print(namecol)
   
   if(is.null(tmp$returnfile)){
     returnFile=FALSE
@@ -54,8 +82,11 @@ function(req) {
   
   #print(shp)
   #print(shp$data$filename)
-  shp2 = read_shapefiles(shp = shp, namecol = namecol)
-  feats = generate_features(shp2)
+  #namecol = "GEOID"
+  shp3 = read_shapefiles(shp = shp2, namecol = namecol)
+  feats = generate_features(shp3)
+  idx = apply(feats, 2, FUN=function(x) any(is.na(x)))
+  feats = feats[,!idx]
   preds = generate_predictions(features=feats, namecol = namecol)
   if(returnFile == FALSE){
     list(preds)
@@ -66,6 +97,17 @@ function(req) {
 
 
 ## to post:
-## curl -X POST --form data=@D:/Github/compactness_api/evenlyspaced20_v2.shp --form namecol=GEOID https://compactness.herokuapp.com/api/compact
-## curl -X POST --form data=@D:/Github/compactness_api/evenlyspaced20_v2.shp --form namecol=GEOID http://localhost:6831/compact
+## curl -X POST --form shp=@D:/Github/compactness_api/evenlyspaced20_v2.shp --form shx=@D:/Github/compactness_api/evenlyspaced20_v2.shx --form dbf=@D:/Github/compactness_api/evenlyspaced20_v2.dbf --form prj=@D:/Github/compactness_api/evenlyspaced20_v2.prj--form namecol="district" https://compactness.herokuapp.com/api/compact
+## curl -X POST --form shp=@D:/Github/compactness_api/evenlyspaced20_v2.shp --form shx=@D:/Github/compactness_api/evenlyspaced20_v2.shx --form dbf=@D:/Github/compactness_api/evenlyspaced20_v2.dbf --form prj=@D:/Github/compactness_api/evenlyspaced20_v2.prj --form namecol="GEOID" http://localhost:7571/compact
 ## to check logs: https://dashboard.heroku.com/apps/compactness/logs
+
+
+#test = read_shapefiles("D:/Github/compactness_api/evenlyspaced20_v2.shp", namecol="GEOID")
+#test = read_shapefiles("C:/Users/inter/AppData/Local/Temp/RtmpIDcwt7/evenlyspaced20_v2.shp", namecol="GEOID")
+
+
+## Changes to make:
+#1) Add a param decorator to the method: '#* @param shp:file'?
+#2) There is no filename (probably). You need to generate filename
+#    in the API that is random so people don't overwrite each other.
+#    Then, write to that file using $shp[1] (data uploaded) -- uuid library
